efd9ef668027cf33739eb87bcd79bd6f103ec410
Buxiaoyue <16011016@buaa.edu.cn>
2024-12-22 02:36:51 +0000

    version 1.0.0, 2014.12.22

--- exp/kkd/Ni_loop/dislocation.bat
+++
@@ -1,4 +0,0 @@
-atomsk Ni_crystal.lmp cfg
-atomsk Ni_crystal.cfg -dislocation loop 0.5*box 0.5*box 0.5*box X 20 2.489 0 0 0.33 Ni_loop.cfg
-atomsk Ni_loop.cfg lmp
-@REM plane_normal radius_in_Angstrom Burgers_vector Poisson_ratio
